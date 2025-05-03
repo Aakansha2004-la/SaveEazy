be2bda5d7e83e9938c262ab9052980d4d9827df2
Aakansha2004-la <aakanshapillai20@gmail.com>
2025-05-03 10:46:29 +0530

    Updated main.py with new features

--- frontend/main.py
+++ frontend/main.py
@@ -4,7 +4,7 @@ import requests
 import altair as alt
 from datetime import datetime
 
-API_URL = "https://saveeazy.onrender.com"
+API_URL = "https://saveeazy.onrender.com/api"
 
 st.set_page_config(page_title="SaveEazy", page_icon="💰", layout="wide")
 st.title("SaveEazy")
